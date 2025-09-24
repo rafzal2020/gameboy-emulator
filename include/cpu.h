9bf7255635114ed22b4b7cec6a2ef4e02489893d
Rayaan <69915472+rafzal2020@users.noreply.github.com>
2025-09-24 16:45:10 -0400

    New version

---
+++ include/cpu.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <common.h>
+#include <instructions.h>
+
+typedef struct {
+    u8 a;
+    u8 f;
+    u8 b;
+    u8 c;
+    u8 d;
+    u8 e;
+    u8 h;
+    u8 l;
+    u16 sp;
+    u16 pc;
+} cpu_registers;
+
+typedef struct {
+    cpu_registers regs;
+
+    u16 fetched_data;
+    u16 mem_dest;
+    bool dest_is_mem;
+    u8 cur_opcode;
+    instruction *cur_inst;
+
+    bool halted;
+    bool stepping;
+} cpu_context;
+
+
+void cpu_init();
+bool cpu_step();
